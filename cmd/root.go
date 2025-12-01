9681e65cd5f4a13caf299d48bbab4b3583deac49
Juanma Roca <juanmaxroca@gmail.com>
2025-11-30 22:23:17 -0500

    fix[root]: description update

--- cmd/root.go
+++ cmd/root.go
@@ -12,8 +12,8 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "helm",
-	Short: "Helm - A minimalistic TUI Pomodoro timer",
-	Long:  "Helm is a minimalistic TUI Pomodoro timer designed for pure focus",
+	Short: "Helm - A minimalistic TUI timer",
+	Long:  "Helm is a minimalistic TUI timer designed for pure focus",
 	RunE: func(_ *cobra.Command, _ []string) error {
 		p := tea.NewProgram(tui.NewModel(), tea.WithAltScreen())
 		_, err := p.Run()
