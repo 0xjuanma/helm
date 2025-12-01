864a645160d6290c806b65cf6c92ed4162e73a5a
Juanma Roca <juanmaxroca@gmail.com>
2025-11-30 21:11:27 -0500

    feat[root]: toot command

--- cmd/root.go
+++ cmd/root.go
@@ -4,21 +4,26 @@ import (
 	"fmt"
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
+
+	"github.com/0xjuanma/helm/internal/tui"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "helm",
-	Short: "Helm",
+	Short: "Helm - A minimalistic TUI Pomodoro timer",
 	Long:  "Helm is a minimalistic TUI Pomodoro timer designed for pure focus",
-	Run: func(_ *cobra.Command, _ []string) {
-		fmt.Println("Helm 0.1.0")
+	RunE: func(_ *cobra.Command, _ []string) error {
+		p := tea.NewProgram(tui.NewModel(), tea.WithAltScreen())
+		_, err := p.Run()
+		return err
 	},
 }
 
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
